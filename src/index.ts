286d6c06889318a709caf9bfc55957ed92939abf
Lakmal98 <lakmalepp@gmail.com>
2021-11-01 23:05:36 +0530

    published 1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-// Hello world function
-function helloWorld() {
-  console.log("Hello world");
-}
+import * as fs from "fs";
 
-helloWorld();
+export class Logger {
+  private file: string;
+  private name: string;
+
+  constructor(name: string = "log", file: string = "logs.log") {
+    this.file = file;
+    this.name = name;
+  }
+
+  private write(message: string) {
+    fs.appendFile(
+      this.file,
+      `${this.getDate()} [${this.name}] => ${message}\n`,
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  private getDate() {
+    const date = new Date();
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  }
+
+  public log(message: string) {
+    this.write(message);
+  }
+}
