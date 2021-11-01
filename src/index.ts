36535e78ef82d52f16dc1b48b5ae872f3b3b5db9
Lakmal98 <lakmalepp@gmail.com>
2021-11-02 00:45:33 +0530

    New version

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ export class Logger {
   private file: string;
   private name: string;
 
-  constructor(name: string = "log", file: string = "logs.log") {
+  constructor({
+    name = "log",
+    file = "logs.log",
+  }: { name?: string; file?: string } = {}) {
     this.file = file;
     this.name = name;
   }
 
-  private write(message: string) {
+  private write({ message, type }: { message: string; type: string }): void {
     fs.appendFile(
       this.file,
-      `${this.getDate()} [${this.name}] => ${message}\n`,
+      `${this.getTimeStamp()} [${type}] [${this.name}] >> ${message}\n`,
       (err) => {
         if (err) {
           console.log(err);
@@ -21,12 +24,27 @@ export class Logger {
     );
   }
 
-  private getDate() {
-    const date = new Date();
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  private getTimeStamp(): string {
+    return new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
   }
 
-  public log(message: string) {
-    this.write(message);
+  public error(message: string): void {
+    this.write({ message, type: "Error  " });
+  }
+
+  public info(message: string): void {
+    this.write({ message, type: "Info   " });
+  }
+
+  public debug(message: string): void {
+    this.write({ message, type: "Debug  " });
+  }
+
+  public success(message: string): void {
+    this.write({ message, type: "Success" });
+  }
+
+  public fatal(message: string): void {
+    this.write({ message, type: "Fatal  " });
   }
 }
