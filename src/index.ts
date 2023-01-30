bd6298f4156019bdf798345d395e312770e7ce61
Dimuthu Lakmal <lakmalepp@gmail.com>
2023-01-31 03:11:59 +0530

    add linting

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,56 @@
-import * as fs from "fs";
+import * as fs from 'fs'
 
 export class Logger {
-  private file: string;
-  private name: string;
+  private readonly file: string
+  private readonly name: string
 
-  constructor(name: string = "log", file: string = "logs.log") {
-    this.file = file;
-    this.name = name;
+  constructor (name: string = 'log', file: string = 'logs.log') {
+    this.file = file
+    this.name = name
   }
 
-  private write({ message, type }: { message: string; type: string }): void {
+  private write ({ message, type }: { message: string, type: string }): void {
     fs.appendFile(
       this.file,
       `${this.getTimeStamp()} [${type}] [${this.name}] >> ${message}\n`,
       (err) => {
-        if (err) {
-          console.log(err);
+        if (err != null) {
+          console.log(err)
         }
       }
-    );
+    )
   }
 
-  private getTimeStamp(): string {
-    return new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+  private getTimeStamp (): string {
+    return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
   }
 
-  public error(message: string): void {
-    this.write({ message, type: "Error  " });
+  public error (message: string): void {
+    this.write({ message, type: 'Error  ' })
   }
 
-  public info(message: string): void {
-    this.write({ message, type: "Info   " });
+  public info (message: string): void {
+    this.write({ message, type: 'Info   ' })
   }
 
-  public debug(message: string): void {
-    this.write({ message, type: "Debug  " });
+  public debug (message: string): void {
+    this.write({ message, type: 'Debug  ' })
   }
 
-  public success(message: string): void {
-    this.write({ message, type: "Success" });
+  public success (message: string): void {
+    this.write({ message, type: 'Success' })
   }
 
-  public fatal(message: string): void {
-    this.write({ message, type: "Fatal  " });
+  public fatal (message: string): void {
+    this.write({ message, type: 'Fatal  ' })
   }
 
   // append custom message to the log file
-  public append(message: string): void {
+  public append (message: string): void {
     fs.appendFile(this.file, message, (err) => {
-      if (err) {
-        console.log(err);
+      if (err != null) {
+        console.log(err)
       }
-    });
+    })
   }
 }
